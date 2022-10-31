f3eee4fda60f0f6178d90d758302fbb84e9d0154
mahimagupta24 <mahimagupta2401@gmail.com>
2022-10-31 11:41:16 +0530

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import unsplash from "../api/unsplash";
+import ImageList from "./ImageList";
+import SearchBar from "./SearchBar";
 
-const App=()=>{
+// const App=()=>{
+class App extends React.Component {
+  state = { images: [] };
+  //   async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
+    const resp = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+    // console.log(resp.data.results)
+    this.setState({ images: resp.data.results }); //it will cause images to re render
+  };
+
+  // console.log(term)
+  render() {
     return (
-    <div className='ui container'style={{marginTop:'10px'}}>
-        <SearchBar />
-    </div>
-    )
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images}/>
+        {/* Found:{this.state.images.length} images */}
+      </div>
+    );
+  }
 }
 export default App;
